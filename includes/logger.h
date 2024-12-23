cc05192ca8a90232b4b0046cba108dcab5009ed4
Marko Z. Muc <marko.muc12@gmail.com>
2024-12-23 21:40:52 +0100

    feat: added LOG_PANIC macro.

--- includes/logger.h
+++ includes/logger.h
@@ -2,12 +2,19 @@
 #include <stdio.h>
 #endif
 
-#define LOG(...)                                                               \
-  do {                                                                         \
-    fprintf(stdout, __VA_ARGS__);                                              \
-  } while (0)
+#define LOG(...)                  \
+  do {                            \
+    fprintf(stdout, __VA_ARGS__); \
+  } while(0)
 
-#define LOG_ERR(...)                                                           \
-  do {                                                                         \
-    fprintf(stderr, __VA_ARGS__);                                              \
-  } while (0)
+#define LOG_ERR(...)              \
+  do {                            \
+    fprintf(stderr, __VA_ARGS__); \
+  } while(0)
+
+#define LOG_PANIC(...)            \
+  do {                            \
+    fprintf(stderr, "%s:%d: Panicked in function %s with error:\n",__FILE__, __LINE__, __func__); \
+    fprintf(stderr, __VA_ARGS__); \
+    exit(1);                      \
+  } while(0)
