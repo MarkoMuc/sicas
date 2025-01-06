2655ff81107859f565ea5151493595f1b92088f2
Marko Z. Muc <marko.muc12@gmail.com>
2025-01-06 21:41:26 +0100

    feat(assembler): implemented function formats 1,2,3

--- includes/assembler.h
+++ includes/assembler.h
@@ -21,6 +21,21 @@
 // macros
 
 #define ASSEMBLER_BODY_LINE 60
+#define ASSEMBLER_INSTR_MAX_SIZE 4
+#define INSTR_B1 1
+#define INSTR_B2 2
+#define INSTR_B3 3
+#define INSTR_B4 4
+#define NI_SIC_BITS 0x0
+#define NI_IMMEDIATE_BITS 0x1
+#define NI_INDIRECT_BITS 0x2
+#define NI_SICXE_BITS 0x3
+
+#define INDEX_BIT 0x1
+#define BASE_REL_BITS 0x2
+#define PC_REL_BITS 0x1
+#define FORMAT_BIT 0x1
+#define BIT_OFF 0x0
 
 // mfunc
 
--- src/assembler.c
+++ src/assembler.c
@@ -5,7 +5,7 @@
 
 void assemble_instructions(InstrVector *instrs, SymTable *sym, FILE *output){
   assemble_header(instrs, sym, output);
-  assemble_body(instrs, sym, output);
+ // assemble_body(instrs, sym, output);
   assemble_end(instrs, sym, output);
 }
 
@@ -25,70 +25,109 @@ void assemble_header(InstrVector *instrs, SymTable *sym, FILE *output){
 }
 
 void assemble_body(InstrVector *instrs, SymTable *sym, FILE *output){
- // char *body = malloc(sizeof(*body) * (ASSEMBLER_BODY_LINE + 1));
- // size_t instr_count = instrs->count;
+  uint8_t *body = malloc(sizeof(*body) * (ASSEMBLER_BODY_LINE + 1));
+  size_t instr_count = instrs->count;
  // size_t b_idx = 0;
- // uint64_t start_addr = 0;
- // uint64_t new_addr = 0;
- // bool reserved = false;
+  uint64_t start_addr = 0;
+  uint32_t pc_reg = 0;
+  uint32_t base_reg = 0;
+  //bool reserved = false;
+  //bool endline = false;
 
- // if(body) {
- //   LOG_PANIC("Failed to allocate memory for a string.");
- // }
+  if(body) {
+    LOG_PANIC("Failed to allocate memory for a string.");
+  }
 
- // for(size_t i = 0; i < instr_count; i++){
- //   Instruction *instr = instrvec_get(instrs, i);
+  if(instr_count < 1) {
+    LOG_XERR("No instructions found.\n");
+  }
 
- //   if(instr->type == DIRECTIVE){
- //     continue;
- //   }else if (instr->type == RMEM) {
- //     body[b_idx] = '\0';
- //     new_addr += calc_offset();
- //     reserved = true;
- //     continue;
- //   }else if(instr->type == IMEM){
+  size_t i = 0;
+  Instruction *instr = instrvec_get(instrs, i++);
+  uint8_t byte_rep[ASSEMBLER_INSTR_MAX_SIZE] = {0, 0, 0, 0}; 
+  //uint8_t carry_bits = 0;
+  start_addr = instr->addr;
+  pc_reg = start_addr;
 
- //   }else if(instr->type == INSTR){
- //     Token *tk = tokvec_get(instr->vec, 0);
- //     uint8_t opcode = mnemonic_get_opcode(tk->type);
+  while(i < instr_count) {
+    if(instr->type == DIRECTIVE) {
+      //TODO: set base register
+      continue;
+    }
 
- //     if(instr->format == TWO) {
- //       sprintf(body + b_idx, "%02xl", opcode);
- //       b_idx = b_idx + 2;
+    if(instr->type == INSTR) {
+      MInstr *operation = (MInstr *)instr->instr;
+      byte_rep[INSTR_B1] = mnemonic_get_opcode(operation->op);
+      pc_reg += instr->format;
 
- //       tk = tokvec_get(instr->vec, 1);
- //       uint8_t reg1 = mnemonic_get_reg(tk->str);
+      if(instr->format == TWO) {
+        Regs *regs = (Regs *)operation->oper;
+        byte_rep[INSTR_B2] = regs->reg1 << 4 | regs->reg2;
+      }else if(instr->format == THREE){
+        Mem *mem = (Mem *)operation->oper;
+        uint8_t bit_flags = NI_SICXE_BITS;
+        uint32_t sym_addr = 0;
 
- //       tk = tokvec_get(instr->vec, 2);
- //       uint8_t reg2 = mnemonic_get_reg(tk->str);
+        if(mem->tk->type == ID) {
+          SymValue *val = symtab_get_symbol(sym, mem->tk->str);
 
- //       sprintf(body + b_idx, "%01x%01x", reg1, reg2);
- //       b_idx = b_idx + 2;
- //     }else if(instr->format == THREE){
- //       opcode = opcode | 0b11;
- //       sprintf(body + b_idx, "%02xl", opcode);
- //       b_idx = b_idx + 2;
+          if(!val){
+            LOG_XERR("Symbol %s has not been defined yet.", mem->tk->str);
+          }
 
- //     }else if(instr->format == FOUR){
+          if(val->set == false) {
+            LOG_XERR("Symbol %s has not been defined yet.", mem->tk->str);
+          }
 
- //     }
+          sym_addr = val->addr;
+        }else {
+          sym_addr = token_to_long(mem->tk);
+        }
 
- //     new_addr = new_addr + instr->format;
- //   }
+        if(mem->mem_type == IMM) {
+          bit_flags = NI_IMMEDIATE_BITS;
+        }else if(mem->mem_type == IND) {
+          bit_flags = NI_INDIRECT_BITS;
+        }
 
- //   if(reserved || b_idx == ASSEMBLER_BODY_LINE) {
- //     body[b_idx] = '\0';
- //     fprintf(output, "T%06lx%02lx%s\n", start_addr, new_addr - start_addr, body);
- //     b_idx = 0;
- //     start_addr = new_addr;
- //     reserved = false;
- //   }
+        //TODO: Do i need to switch by 2?
+        byte_rep[INSTR_B1] = (byte_rep[INSTR_B1] << 2) | bit_flags;
 
- // }
+        bit_flags = (mem->indexed ? INDEX_BIT : BIT_OFF) << 2;
+        int32_t diff = pc_reg - sym_addr;
 
-  //free(body);
+        if((diff < 0 && diff >= -2048) || (diff >= 0 && diff <= 2047)){
+          bit_flags = (bit_flags | PC_REL_BITS) << 1; 
+        }else {
+          if (diff > 0 && diff <= 4095){
+            diff = base_reg - sym_addr;
+            bit_flags = (bit_flags | BASE_REL_BITS) << 1; 
+          } else{
+            LOG_XERR("Instruction cannot be used with PC or BASE register addressing.\n");
+          }
+        }
+        bit_flags = (bit_flags | BIT_OFF) << 4;
+
+        // Use the upper 4 bytes and make sure to zero out all other values.
+        // FIXME: check if this correctly works with two's complement
+        bit_flags = bit_flags | ((diff >> 8) & (0x1 << 4));
+        byte_rep[INSTR_B2] = bit_flags;
+
+        // Only use the lower 8 bytes
+        diff = (0x1 << 8) & diff;
+        byte_rep[INSTR_B3] = diff;
+
+      }else if(instr->format == FOUR){
+        //bp == 0
+      }
+
+    }
+
+  }
+
+ free(body);
 }
 
 void assemble_end(InstrVector *instrs, SymTable *sym, FILE *output){
-  fprintf(output, "E%06lx", instrs->end_addr);
+  fprintf(output, "E%06lx\n", instrs->end_addr);
 }
