153c3e71fc35186110b0b1475029e26e14e422a4
Marko Z. Muc <marko.muc12@gmail.com>
2025-01-14 22:10:47 +0100

    misc: nicer location logging

--- includes/logger.h
+++ includes/logger.h
@@ -29,12 +29,12 @@
 
 #define LOG_LERR(sl, l,...) \
   do {                      \
-    fprintf(stderr, "[%ld, %ld]:[%ld, %ld] ", (sl).s_row, (sl).s_col, (l).e_row, (l).e_col);\
+    fprintf(stderr, "[%ld,%ld]:[%ld,%ld] ", (sl).s_row, (sl).s_col, (l).e_row, (l).e_col);\
     LOG_ERR(__VA_ARGS__);\
   } while(0);
 
 #define LOG_XLERR(sl, l,...) \
   do {                      \
-    fprintf(stderr, "[%ld, %ld]:[%ld, %ld] ", (sl).s_row, (sl).s_col, (l).e_row, (l).e_col);\
+    fprintf(stderr, "[%ld,%ld]:[%ld,%ld] ", (sl).s_row, (sl).s_col, (l).e_row, (l).e_col);\
     LOG_XERR(__VA_ARGS__);\
   } while(0);
